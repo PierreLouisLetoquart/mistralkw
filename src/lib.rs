c9baaae123b76db5533bd5d23494b310ceedd1aa
Random Long Int <pierrelouislet@gmail.com>
2024-02-02 18:23:57 -0500

    refactor: create lib.rs + test mod

---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+use anyhow::{bail, Context, Result};
+use clap::Parser;
+use ollama_rs::{generation::completion::request::GenerationRequest, models::LocalModel, Ollama};
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[arg(short, long)]
+    model_name: String,
+
+    #[arg(short, long, value_name = "FILE")]
+    document_path: PathBuf,
+}
+
+pub async fn gen_keywords(args: &Cli, ollama: &Ollama) -> Result<()> {
+    let models_available = list_local_models(&ollama).await?;
+
+    check_model_availability(&args.model_name, &models_available)?;
+
+    let content = fs::read_to_string(&args.document_path)
+        .with_context(|| format!("Unable to read the file: {:?}", &args.document_path))?;
+
+    let res = ollama
+        .generate(GenerationRequest::new(args.model_name.to_string(), content))
+        .await
+        .map_err(|err| anyhow::anyhow!("Unable to generate the document: {}", err))?;
+
+    println!("{}", res.response);
+
+    Ok(())
+}
+
+async fn list_local_models(ollama: &Ollama) -> Result<Vec<LocalModel>> {
+    ollama
+        .list_local_models()
+        .await
+        .with_context(|| "Unable to list local models")
+}
+
+fn check_model_availability(model_name: &str, models_available: &[LocalModel]) -> Result<()> {
+    if !models_available
+        .iter()
+        .any(|model| model.name == model_name)
+    {
+        bail!("Model '{}' is not available.", model_name);
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_gen_keywords() {
+        let args = Cli {
+            model_name: "test".to_string(),
+            document_path: PathBuf::from("test.txt"),
+        };
+        let ollama = Ollama::default();
+
+        let res = gen_keywords(&args, &ollama).await;
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_check_model_availability() {
+        let models = vec![
+            LocalModel {
+                name: "test".to_string(),
+                modified_at: "".to_string(),
+                size: 0,
+            },
+            LocalModel {
+                name: "test2".to_string(),
+                modified_at: "".to_string(),
+                size: 0,
+            },
+        ];
+
+        let res = check_model_availability("test", &models);
+        assert!(res.is_ok());
+
+        let res = check_model_availability("test3", &models);
+        assert!(res.is_err());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,15 @@
-use anyhow::{bail, Context, Result};
 use clap::Parser;
-use ollama_rs::{generation::completion::request::GenerationRequest, Ollama};
-use std::fs;
-use std::path::PathBuf;
+use mixtralkw::{gen_keywords, Cli};
+use ollama_rs::Ollama;
 use std::process;
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    #[arg(short, long)]
-    model_name: String,
-
-    #[arg(short, long, value_name = "FILE")]
-    document_path: PathBuf,
-}
 
 #[tokio::main]
 async fn main() {
-    if let Err(err) = run().await {
+    let args = Cli::parse();
+    let ollama = Ollama::default();
+
+    if let Err(err) = gen_keywords(&args, &ollama).await {
         eprintln!("[ERROR] {}", err);
         process::exit(1);
     }
-}
-
-async fn run() -> Result<()> {
-    let args = Cli::parse();
-
-    let ollama = Ollama::default();
-
-    // ollama list and verify if the model exists...
-    let models_available = ollama
-        .list_local_models()
-        .await
-        .with_context(|| "Unable to list local models")?;
-
-    let model_exists = models_available
-        .iter()
-        .any(|model| model.name == args.model_name);
-
-    if !model_exists {
-        bail!("Model '{}' is not available.", args.model_name);
-    }
-
-    let content = fs::read_to_string(&args.document_path)
-        .with_context(|| format!("Unable to read the file: {:?}", args.document_path))?;
-
-    println!("[INFO] Scanning the document...");
-    let res = ollama
-        .generate(GenerationRequest::new(args.model_name, content))
-        .await
-        .map_err(|err| anyhow::anyhow!("Unable to generate the document: {}", err))?;
-
-    println!("[INFO] Document scanned successfully");
-    println!("[RESP]\n\n{}\n\n[ENDRESP]", res.response);
-
-    Ok(())
 }
