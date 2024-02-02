18c6347628787e4e91cb178ff60e81a352bbe9c2
Random Long Int <pierrelouislet@gmail.com>
2024-02-02 17:19:50 -0500

    feat: add clap-rs

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,50 @@
 use ollama_rs::{generation::completion::request::GenerationRequest, models::LocalModel, Ollama};
-use std::env;
 use std::fs;
 use std::process;
 
-fn help(program: &str) {
-    eprintln!("[ERROR] This program required 2 args");
-    println!("[USAGE] {} <model-name> <document-path>", program);
-    process::exit(1);
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    model_name: String,
+
+    #[arg(short, long, value_name = "FILE")]
+    document_path: PathBuf,
 }
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    match args.len() {
-        3 => println!(
-            "[INFO] Scanning {} using the {} model",
-            &args[2], &args[1]
-        ),
-        _ => help(&args[0]),
-    }
-
-    let model_name = &args[1];
-    let file_path = &args[2];
+    let args = Cli::parse();
 
     let ollama = Ollama::default();
 
     // ollama list and verify if the model exists...
-    let models_available: Vec<LocalModel> = ollama.list_local_models().await.unwrap_or_else(|err| {
-        eprintln!("[ERROR] Unable to list local models: {}", err);
-        process::exit(1);
-    });
+    let models_available: Vec<LocalModel> =
+        ollama.list_local_models().await.unwrap_or_else(|err| {
+            eprintln!("[ERROR] Unable to list local models: {}", err);
+            process::exit(1);
+        });
 
-    let model_exists = models_available.iter().any(|model| model.name == model_name.to_string());
+    let model_exists = models_available
+        .iter()
+        .any(|model| model.name == args.model_name);
 
     if !model_exists {
-        eprintln!("[ERROR] Model '{}' is not available.", model_name);
+        eprintln!("[ERROR] Model '{}' is not available.", args.model_name);
         process::exit(1);
     }
 
-    let content = fs::read_to_string(file_path).unwrap_or_else(|err| {
+    let content = fs::read_to_string(args.document_path).unwrap_or_else(|err| {
         eprintln!("[ERROR] Unable to read the file: {}", err);
         process::exit(1);
     });
 
     println!("[INFO] Scanning the document...");
     let res = ollama
-        .generate(GenerationRequest::new(model_name.into(), content))
+        .generate(GenerationRequest::new(args.model_name, content))
         .await;
 
     if let Ok(res) = res {
